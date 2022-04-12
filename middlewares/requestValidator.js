45e936dbd5574836a26b43ba7ae8b83ac7f0b433
Ahmad Fauzi <56423610+afauziaf@users.noreply.github.com>
2022-04-12 09:44:22 +0700

    Update requestValidator.js

--- middlewares/requestValidator.js
+++ middlewares/requestValidator.js
@@ -2,6 +2,7 @@ import { validationResult } from 'express-validator'
 import response from './../response.js'
 
 const validate = (req, res, next) => {
+    console.log(req);
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
